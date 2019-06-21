f76e7d439847b62c10bde69d86e37a9f0f0de5a5
Michel Engelen <michel@nomorejs.net>
2019-06-21 10:15:29 +0200

    fixed a bug with authentification

--- src/variables/constants/index.js
+++ src/variables/constants/index.js
@@ -1,3 +1,16 @@
+import { firebase } from './config';
+import { firebaseLocal } from './localConfig';
+
+console.log('ENV variable: ', process.env);
+
+let CONFIG = firebaseLocal;
+if (firebaseLocal && process.env.REACT_APP_ENV === 'local') {
+  CONFIG = firebaseLocal;
+} else {
+  CONFIG = firebase;
+}
+
+export { CONFIG };
+
 export * from './icons';
 export * from './variables';
-export * from './config';
